0ba7f51cd177f3d49f053fb0058348936075da67
JinSheng <dhorse443@gmail.com>
2022-02-20 14:33:18 -0700

    built table with filtered data

--- src/compoments/Table/index.js
+++ src/compoments/Table/index.js
@@ -1,8 +1,49 @@
-import React from 'react';
-import '../../styles.css';
+import React, { useState } from "react";
+import "../../styles.css";
 
-const Table = () => {
-  return <h3 className="footer">&copy; 2021-02-20 John Chang</h3>;
+const Table = ({ props }) => {
+  const movies = props;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [entriesPerPage] = useState(10);
+
+  // Gets current page array
+  const indexOfLastPage = currentPage * entriesPerPage;
+  const indexOfFirstPage = indexOfLastPage - entriesPerPage;
+  const currentEntries = movies.slice(indexOfFirstPage, indexOfLastPage);
+
+  // Changes page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  if (!movies.length > 0) {
+    return (
+      <>
+        <h2>Movie Results:</h2>
+        <h2>No Results Found.</h2>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h2> Movie Results:</h2>
+        <table>
+          <tbody>
+            <tr>
+              <td>Title</td>
+              <td>Image</td>
+            </tr>
+            {currentEntries.map((movie, index) => {
+              return (
+                <tr key={index}>
+                  <td>{movie.title}</td>
+                  <td>"Image"</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </>
+    );
+  }
 };
 
 export default Table;
