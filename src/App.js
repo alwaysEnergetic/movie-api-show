6418d82bb364c8a9a30cbb832f10882141cd0355
JinSheng <dhorse443@gmail.com>
2022-02-20 22:39:34 -0700

    whole background color set with index.css

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import Header from "./compoments/Header";
 import Main from "./compoments/Main";
 import Footer from "./compoments/Footer";
+import "./App.css";
 import "./styles.css";
-
 function App() {
   return (
     <div className="App">
